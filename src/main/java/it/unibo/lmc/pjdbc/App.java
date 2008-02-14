3ede66929365cc0fe61797853dab27d580883f85
Stefano Tamagnini <yoghi@users.noreply.github.com>
2008-02-14 16:48:45 +0000

    ** mvn project setup

---
+++ src/main/java/it/unibo/lmc/pjdbc/App.java
@@ -0,0 +1,13 @@
+package it.unibo.lmc.pjdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
